334c3f6a06d48152f753447b71bc54c1a59fb874
osemenjoy <osemenjoy448@gmail.com>
2025-05-26 13:10:29 +0100

    host issue

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["https://habit-tracker-jryo.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["habit-tracker-jryo.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
